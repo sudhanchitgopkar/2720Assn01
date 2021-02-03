7805cab583b19904e7262e6ce2356638d8e7bea3
Kewal Kalsi <ksk44796@uga.edu>
2021-02-03 01:35:39 -0500

    Added methods and data members to Student.h and Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,65 @@
+#include Student.h
+
+Student::Student() {
+
+  fullName = "";
+  username = "";
+  password = "";
+  
+  projectGrade = 0;
+  quizGrade = 0;
+  midtermGrade = 0;
+  finalGrade = 0.0;
+  
+}
+
+Student::Student(string name, string username, string password, int projectGrade, int quizGrade, int midtermGrade, int finalGrade) {
+  fullName = name;
+  this->username = username;
+  this->password = password;
+  
+  this->projectGrade = projectGrade;
+  this->quizGrade = quizGrade;
+  this->midtermGrade = midtermGrade;
+  this->finalGrade = finalGrade;
+}
+
+bool Student::login(string username, string password) {
+  if (this->username == username && this->password == password) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+string Student::getStudentName() {
+  return this->fullName;
+}
+
+int Student::getProjectGrade() {
+  return this->projectGrade;
+}
+
+int Student::getQuizGrade() {
+  return this->quizGrade;
+}
+
+int Student::getMidtermGrade() {
+  return this->midtermGrade;
+}
+
+int Student::getFinalGrade() {
+  return this->finalGradel
+}
+
+double Student::getOverallGrade() {
+  return ((0.3 * getProjectGrade()) + (0.1 * getQuizGrade()) + (0.2 * getMidtermGrade()) + (0.4 * getFinalGrade()));
+}
+
+string Student::getUsername() {
+  return this->username;
+}
+
+string Student::getPassword() {
+  return this->password;
+}
---
+++ Student.h
@@ -0,0 +1,32 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+class Student {
+
+ private:
+  string fullName;
+  string username;
+  string password;
+  
+  int projectGrade;
+  int quizGrade;
+  int midtermGrade;
+  int finalGrade;
+
+ public:
+
+  Student();
+  Student(string name, string username, string password, int projectGrade, int quizGrade, int midtermGrade, int finalGrade);
+  
+  bool login(string username, string password);
+  string getStudentName();
+
+  int getProjectGrade();
+  int getQuizGrade();
+  int getMidtermGrade();
+  int getFinalGrade();
+  double getOverallGrade();
+  string getUsername();
+  string getPassword();
+  
+}
