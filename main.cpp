e1bf57305ccfebfee5dd8eaa69471cf10bc129fe
Kewal Kalsi <ksk44796@uga.edu>
2021-02-03 01:46:43 -0500

    Began main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<unistd.h>
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+  cout << "User types," << endl << "\t1 - Instructor" << endl << "\t2 - Student" << endl;
+  cout << "Select a login user type or enter 3 to exit:" << endl;
+  
+}
